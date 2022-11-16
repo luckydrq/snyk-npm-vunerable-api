8e5a84190c5f142b0f969420095ec3d065aac702
luckydrq <drqzju@gmail.com>
2022-11-16 15:41:40 +0800

    config: change port to 7001

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import app from './lib/app';
 
-const PORT = process.env.PORT || 3389;
+const PORT = process.env.PORT || 7001;
 
 app.listen(PORT, async () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}/`);
