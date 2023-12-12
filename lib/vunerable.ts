d85521b91cdf46f17950f3e6557d65f7e48a367f
luckydrq <drqzju@gmail.com>
2023-12-12 17:56:55 +0800

    feat: add timeout for child process

--- lib/vunerable.ts
+++ lib/vunerable.ts
@@ -42,21 +42,23 @@ function getPackageVunerableData(json: Record<string, any>) {
       version: v.version,
       severity: v.severity,
       fixedIn: v.fixedIn,
-      upgradePath: v.upgradePath,
-    })),
+      upgradePath: v.upgradePath
+    }))
   };
 }
 
-async function queryPackageVunerabilities(pkg: string, version: string, jsonFile: string): Promise<Record<string, any>> { 
+async function queryPackageVunerabilities(pkg: string, version: string, jsonFile: string): Promise<Record<string, any>> {
   return await new Promise((resolve, reject) => {
-    const cp = spawn('snyk', ['test', `${pkg}@${version}`, '--json', `--json-file-output=${jsonFile}`]);
+    const cp = spawn('snyk', ['test', `${pkg}@${version}`, '--json', `--json-file-output=${jsonFile}`], {
+      timeout: 5 * 1000
+    });
 
     let result = '';
-    cp.stdout.on('data', (data) => {
+    cp.stdout.on('data', data => {
       result += data.toString();
     });
 
-    cp.on('close', (code) => {
+    cp.on('close', code => {
       try {
         const jsonResult = JSON.parse(result as string);
         resolve(jsonResult);
